126fb3cb2b91ff9ddb39b360f4d0cd166460669d
SneG95 <Dima6233@yandex.ru>
2023-01-28 13:35:16 +0300

    Использует паттерн Наблюдатель

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -1,8 +1,9 @@
+import Observable from '../framework/observable.js';
 import { getPoint } from '../mock/point.js';
 
 const POINT_COUNT = 3;
 
-export default class PointsModel {
+export default class PointsModel extends Observable {
   #points = Array.from({length: POINT_COUNT}, getPoint);
 
   get points() {
--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -21,6 +21,10 @@ export default class BoardPresenter {
     this.#pointsModel = pointsModel;
   }
 
+  get points() {
+    return this.#pointsModel.points;
+  }
+
   init() {
     this.#boardPoints = [...this.#pointsModel.points].sort(sortPointDateDown);
     this.#sourcedPointsList = [...this.#pointsModel.points].sort(sortPointDateDown);
