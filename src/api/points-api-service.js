c9ef2b6e1e68d03044cde9fa49d2de6977282305
SneG95 <Dima6233@yandex.ru>
2023-02-03 18:11:10 +0300

    Создаст класс для работы с api точек маршрута

---
+++ src/api/points-api-service.js
@@ -0,0 +1,26 @@
+import ApiService from '../framework/api-service.js';
+
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+};
+
+export default class PointsApiService extends ApiService {
+  get points() {
+    return this._load({url: 'points'})
+      .then(ApiService.parseResponse);
+  }
+
+  async updateTask(point) {
+    const response = await this._load({
+      url: `tasks/${point.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(point),
+      headers: new Headers({'Content-Type': 'application/json'}),
+    });
+
+    const parsedResponse = await ApiService.parseResponse(response);
+
+    return parsedResponse;
+  }
+}
